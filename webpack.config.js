f81fc31d059445f0722fd82dac341d4ee8cd7220
xujian <2513573453@qq.com>
2020-08-13 22:42:01 +0800

    配置相关loader

--- webpack.config.js
+++ webpack.config.js
@@ -28,8 +28,30 @@ module.exports = {
           },
         ],
       },
-      // 打包样式文件
+      // 打包css文件
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      //   打包scss文件
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+            },
+          },
+          "sass-loader",
+          "postcss-loader",
+        ],
+      },
+      // 打包less文件
+      {
+        test: /\.less$/,
+        use: ["style-loader", "css-loader", "less-loader", "postcss-loader"],
+      },
+      // 打包字体文件
+      { test: /\.(eot|ttf|svg)$/, use: "file-loader" },
     ],
   },
 };
