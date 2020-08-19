c22f9ad957594eb534c180aecdc7b5d6f7e7d295
xujian <2513573453@qq.com>
2020-08-19 18:42:03 +0800

    add

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-console.log(this === window);
+const p1 = new Promise((resolve, reject) => {
+  reject("第一个错");
+});
+
+const p2 = new Promise((resolve, reject) => {
+  resolve("第二个对----");
+});
+
+Promise.allSettled([p1, p2]).then((results) => {
+  console.log(results);
+});
